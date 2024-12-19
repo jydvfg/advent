d5f82c511ca6e49c0d18421bdcd247a4b948ffa7
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-12-19 13:26:14 +0100

    second commit

--- day-04.py
+++ day-04.py
@@ -9,6 +9,33 @@ S.S.S.S.SS
 ..M.M.M.MM
 .X.X.XMASX"""
 
-pattern = "XMAS"
+lines = data.splitlines()
 
-def
+rows = [list(char) for char in lines]
+
+
+
+pattern = "XMAS"
+backwards_pattern = "SAMX"
+
+pattern_match = 0
+
+def horrizontal_check(dataset):
+    matches = 0
+    for i in dataset:
+        for j in range(len(i) - 3):
+            result = ''.join(i[j:j+4])
+            if result == pattern:
+                matches += 1
+ 
+        for j in range(len(i) - 1, 2, -1):
+            result_backwards = ''.join(i[j-3:j+1])
+            print(result_backwards)
+            if result_backwards == backwards_pattern:
+                matches += 1
+    
+    return matches
+pattern_match = horrizontal_check(rows)
+
+
+print(pattern_match)
